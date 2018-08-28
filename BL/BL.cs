64dd951806f6764db95936956d106bf43198f205
Yehonatan Avni <yavni1@gmail.com>
2018-08-28 22:03:51 +0300

    Uploading BL

---
+++ BL/BL.cs
@@ -0,0 +1,54 @@
+﻿using DAL;
+using DP;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+   public class BL : IBL
+    {
+        IDAL Dal;
+        DBInfo db;
+        public BL()
+        {
+            Dal = DALFactory.GetInstance();  
+        }
+
+        public void ChangeDatabase(DBInfo db)
+        {
+            Dal.ChangeDataBaseInfo(db);
+        }
+        public DBInfo GetUserInfo()
+        {
+            return Dal.GetCurrentInfo();
+        }
+        public bool CheckUserInfo(DBInfo db)
+        {
+            return Dal.CheckUserInfo(db);
+        }
+        public void SetUserInfo(DBInfo db)
+        {
+            this.db = db;
+        }
+
+        public List<string> GetAllDB()
+        {
+            return Dal.GetAllDB();
+        }
+
+
+        public List<string> GetAllTables()
+        {
+            return Dal.GetAllTables() ;
+        }
+
+        public DataTable GetTable(String table)
+        {
+            return Dal.GetTable(table);
+        }
+    }
+}
---
+++ BL/IBL.cs
@@ -0,0 +1,21 @@
+﻿using DP;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public interface IBL
+    {
+        List<String> GetAllDB();
+        List<String> GetAllTables();
+        DataTable GetTable(String table);
+        DBInfo GetUserInfo();
+        void ChangeDatabase(DBInfo db);
+        void SetUserInfo(DBInfo db);
+        bool CheckUserInfo(DBInfo db);
+    }
+}
