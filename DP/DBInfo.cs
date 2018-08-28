934b04190a28d4786b1219660860a728774b99a5
Yehonatan Avni <yavni1@gmail.com>
2018-08-28 22:05:02 +0300

    Uploading DP

---
+++ DP/DBInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP
+{
+    public class DBInfo
+    {
+        private String username;
+        private String password;
+        private String db;
+        private String server;
+        private String sslMode;
+
+        public string Password { get => password; set => password = value; }
+        public string Db { get => db; set => db = value; }
+        public string Server { get => server; set => server = value; }
+        public string Username { get => username; set => username = value; }
+        public string SslMode { get => sslMode; set => sslMode = value; }
+    }
+}
